38f5c276acded77b93a95e5a74fa0448afeb8fe5
Andre Schloemp <andre.schloemp@me.com>
2014-04-17 22:29:08 +0200

    y

---
+++ Assets/TopCameraControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopCameraControl : MonoBehaviour {
+
+  public GameObject player = null;
+	// Use this for initialization
+	void Start () {
+	  if (player == null) {
+      player = GameObject.Find("Panzer");
+    }
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+    Vector3 lPos = player.transform.position;
+    lPos.y = transform.position.y;
+    transform.position = lPos;
+	}
+}
