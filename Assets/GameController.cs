990b5b04b8b473aba67d6c3219719933271b632f
Andre Schloemp <andre.schloemp@me.com>
2014-04-22 23:13:27 +0200

    e

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -9,11 +9,13 @@ public class GameController : MonoBehaviour
   public string gameName = "Game";
   public int gamePort = 8042;
   public int maxPlayers = 8;
-
   public float scale = 0.5f;
   public float scaleWidth = 10.0f;
   public float scaleHeight = 10.0f;
-
+  public int towerCount = 10;
+  public int generationMode = 0; // 0 = Perlin, 1 = ImprovedPerlin
+  public GameObject towerPreFab = null;
+  public static GameController instance = null;
   protected float[,] fNewHeights;
   protected float[,,] fNewAlphas;
   protected Terrain fTerrain = null;
@@ -22,7 +24,12 @@ public class GameController : MonoBehaviour
   // Use this for initialization
   void Start ()
   {
-    fTerrain = GameObject.Find("Terrain").GetComponent<Terrain> ();
+    if (instance != null) {
+      // raise exception ?
+    }
+    instance = this;
+    towerPreFab = GameObject.Find ("Tower");
+    fTerrain = GameObject.Find ("Terrain").GetComponent<Terrain> ();
     fTerrainData = fTerrain.terrainData;
 
     gameName = PlayerPrefs.GetString ("gameName", gameName);
@@ -35,19 +42,10 @@ public class GameController : MonoBehaviour
     GenerateLevel ();
   }
 
-  void DoPerlin ()
+  void DoAlphaMapByHeights ()
   {
-    Perlin lPerlin = new Perlin ();
-    int lw = fTerrainData.heightmapWidth;
-    int lh = fTerrainData.heightmapHeight;
-    fNewHeights = fTerrainData.GetHeights (0, 0, lw, lh);
-    for (int lx = 0; lx < lw; lx++) {
-      for (int ly = 0; ly < lh; ly++) {
-        fNewHeights [lx, ly] = 0.5f + lPerlin.Noise (lx * scaleWidth / lw, ly * scaleHeight / lh) * scale;
-      }
-    }
-    lw = fTerrainData.alphamapWidth;
-    lh = fTerrainData.alphamapHeight;
+    int lw = fTerrainData.alphamapWidth;
+    int lh = fTerrainData.alphamapHeight;
     fNewAlphas = fTerrainData.GetAlphamaps (0, 0, lw, lh);
     for (int lx = 0; lx < lw; lx++) {
       for (int ly = 0; ly < lh; ly++) {
@@ -63,17 +61,116 @@ public class GameController : MonoBehaviour
     }
   }
 
-  void SetMap() {
-    fTerrainData.SetHeights(0,0,fNewHeights);
-    fTerrainData.SetAlphamaps(0,0,fNewAlphas);
+  void DoPerlin ()
+  {
+    Perlin lPerlin = new Perlin ();
+    int lw = fTerrainData.heightmapWidth;
+    int lh = fTerrainData.heightmapHeight;
+    fNewHeights = fTerrainData.GetHeights (0, 0, lw, lh);
+    for (int lx = 0; lx < lw; lx++) {
+      for (int ly = 0; ly < lh; ly++) {
+        fNewHeights [lx, ly] = 0.5f + lPerlin.Noise (lx * scaleWidth / lw, ly * scaleHeight / lh) * scale;
+      }
+    }
+    DoAlphaMapByHeights ();
+  }
+
+  void DoImprovedPerlin ()
+  {
+    ImprovedPerlin lPerlin = new ImprovedPerlin ();
+    int lw = fTerrainData.heightmapWidth;
+    int lh = fTerrainData.heightmapHeight;
+    fNewHeights = fTerrainData.GetHeights (0, 0, lw, lh);
+    for (int lx = 0; lx < lw; lx++) {
+      for (int ly = 0; ly < lh; ly++) {
+        fNewHeights [lx, ly] = 0.5f + lPerlin.Noise (lx * scaleWidth / lw, ly * scaleHeight / lh, 0.0f) * scale;
+      }
+    }
+    DoAlphaMapByHeights ();
+  }
+  
+  void SetMap ()
+  {
+    fTerrainData.SetHeights (0, 0, fNewHeights);
+    fTerrainData.SetAlphamaps (0, 0, fNewAlphas);
+  }
+
+  void GenerateMap ()
+  {
+    switch (generationMode) {
+    case 0: // Perlin
+      DoPerlin ();
+      break;
+    case 1: // Perlin
+      DoImprovedPerlin ();
+      break;
+    }
+  }
+
+  protected struct __TowerPos
+  {
+    public int x, y;
+    public float h;
+  }
+
+  Vector3 TerrainPosToWorldPos (int aX, int aY)
+  {
+    float ly = fNewHeights [aX, aY];
+    return fTerrain.transform.position + new Vector3 ((float)aX / (float)fTerrainData.heightmapWidth * fTerrainData.size.x, ly * fTerrainData.size.y, (float)aY / (float)fTerrainData.heightmapHeight * fTerrainData.size.z);
+  }
+
+  void GenerateTowers ()
+  {
+    if (towerPreFab != null && towerCount > 0) {
+      int lcount = Mathf.FloorToInt (Mathf.Sqrt (towerCount));
+      int lw = fTerrainData.heightmapWidth;
+      int lh = fTerrainData.heightmapHeight;
+      int lxc = lw / lcount;
+      int lyc = lh / lcount;
+      int lxm = 0;
+      int lym = 0;
+      int lxx = 0;
+      int lyy = 0;
+      __TowerPos[,] lPoss = new __TowerPos[lcount, lcount];
+      for (int lx = 0; lx < lw; lx++) {
+        lym = 0;
+        lyy = 0;
+        for (int ly = 0; ly < lh; ly++) {
+          if (lPoss [lxx, lyy].h < fNewHeights [lx, ly]) {
+            lPoss [lxx, lyy].x = lx;
+            lPoss [lxx, lyy].y = ly;
+            lPoss [lxx, lyy].h = fNewHeights [lx, ly];
+          }
+          lym++;
+          if (lym >= lyc) {
+            lyy++;
+            lym = 0;
+          }
+        }
+        lxm++;
+        if (lxm >= lxc) {
+          lxx++;
+          lxm = 0;
+        }
+      }
+      for (int lx = 0; lx < lcount; lx++) {
+        for (int ly = 0; ly < lcount; ly++) {
+          __TowerPos lPos = lPoss [lx, ly];
+          Vector3 lTPos = TerrainPosToWorldPos (lPos.x, lPos.y) + Vector3.down * 0.5f;
+          print (lPos.x + " " + lPos.y + " " + lPos.h + " " + lTPos);
+          Instantiate(towerPreFab, lTPos, Quaternion.identity);
+        }
+      }
+    }
   }
 
   void GenerateLevel ()
   {
-    DoPerlin();
-    SetMap();
+    GenerateMap ();
+    SetMap ();
+    GenerateTowers ();
   }
-  
+
   // Update is called once per frame
   void Update ()
   {
